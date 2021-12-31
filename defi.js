97e37304df86480428d9036da04cd004db55a12b
Ghasem Kiani <ghasemkiani@gmail.com>
2021-12-31 20:46:39 +0330

    fix error in iwAccount

--- defi.js
+++ defi.js
@@ -6,9 +6,11 @@ import {Client} from "@ghasemkiani/etherscan-api";
 import {iwutil} from "./iwutil.js";
 import {util as utilEth} from "./util.js";
 import {iwscan} from "./iwscan.js";
+import {iwAccount as iwAccountEth} from "./iwAccount.js";
+import {iwContract as iwContractEth} from "./iwContract.js";
 import {iwToken as iwTokenEth} from "./iwToken.js";
 
-class DeFi extends cutil.mixin(Obj, iwutil, iwscan, iwTokenEth) {
+class DeFi extends cutil.mixin(Obj, iwutil, iwscan, iwAccountEth, iwContractEth, iwTokenEth) {
 	
 }
 cutil.extend(DeFi.prototype, {
